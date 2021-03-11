a4a11a23032a05601533198ee6316706e7c155ac
kiliman <kiliman@gmail.com>
2021-03-11 15:30:16 -0500

    ✨ Add ServerTime component which uses loader to get current server time

---
+++ app/routes/components/ServerTime.tsx
@@ -0,0 +1,20 @@
+import { LoaderFunction } from "@remix-run/react";
+import { useComponentData } from "../../utils/useComponentData";
+
+export const loader: LoaderFunction = async () => {
+  return { time: Date.now() };
+};
+
+export default function ServerTime() {
+  const [{ time }, setData, getData] = useComponentData(ServerTime);
+  const handleRefresh = async () => {
+    const { time: newTime } = await getData();
+    setData({ time: newTime });
+  };
+  return (
+    <>
+      <div>Server time is now {time}</div>
+      <button onClick={handleRefresh}>Refresh</button>
+    </>
+  );
+}
