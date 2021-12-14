d35ec1b0f1e55846d97aa4f0d34b67b9633933a8
kiliman <kiliman@gmail.com>
2021-12-14 15:23:28 -0500

    ✨ Add support for component actions and component params

--- app/components/ComponentData.tsx
+++ app/components/ComponentData.tsx
@@ -1,31 +1,66 @@
 import { createContext, useContext } from 'react'
 
-const context = createContext(null)
+type ComponentContextType = {
+  id?: string
+  loaderData?: any
+  actionData?: any
+}
+
+const context = createContext({ id: '', loaderData: null, actionData: null })
 
 export function useComponentData<LoaderType>(): LoaderType | null {
-  const data = useContext(context)
-  return data!['json'] as any
+  const { id, loaderData } = useContext(context)
+  return loaderData?.['json'] as any
+}
+
+export function useComponentId(): string {
+  const { id } = useContext(context)
+  return id
+}
+
+export function useComponentParams(): any {
+  const { id } = useContext(context)
+  return new URLSearchParams(id)
+}
+
+export function useComponentActionData() {
+  const { actionData } = useContext(context)
+  return actionData!
 }
 
 export function useComponentCatch() {
-  const data = useContext(context)
-  return data!['$$caught'] as any
+  const { loaderData } = useContext(context)
+  return loaderData?.['$$caught'] as any
 }
 
-export default function ComponentData({ data, children }: any) {
+function ComponentData({ id, loaderData, actionData, children }: any) {
   let Component = children
 
-  if (data.$$caught) {
+  if (loaderData?.$$caught || actionData?.$$caught) {
     Component = Component.type.CatchBoundary
     return (
-      <context.Provider value={data}>
+      <context.Provider value={{ id, loaderData, actionData }}>
         <Component />
       </context.Provider>
     )
-  } else if (data.$$error) {
+  } else if (loaderData?.$$error || actionData?.$$error) {
     Component = Component.type.ErrorBoundary
-    return <Component error={data.$$error} />
+    return <Component error={loaderData.$$error ?? actionData?.$$error} />
   }
 
-  return <context.Provider value={data}>{Component}</context.Provider>
+  return (
+    <context.Provider value={{ id, loaderData, actionData }}>
+      {Component}
+    </context.Provider>
+  )
 }
+
+ComponentData.Params = function ({ params }: any) {
+  const { id } = useContext(context) as ComponentContextType
+  if (!id) {
+    throw new Error('You must provide an id in <ComponentData/>')
+  }
+  return <input type="hidden" name="$$actionComponentId" value={id} />
+}
+
+export default ComponentData
--- app/utils/remix-component-data.ts
+++ app/utils/remix-component-data.ts
@@ -1,6 +1,13 @@
-export async function callComponentLoaders(loaders: any) {
+import { DataFunctionArgs } from '@remix-run/server-runtime'
+
+export async function callComponentLoaders(
+  args: DataFunctionArgs,
+  loaders: any,
+) {
   const responses = await Promise.all(
-    Object.values(loaders).map(async (loader: any) => callLoader(loader)),
+    Object.entries(loaders).map(async ([key, loader]) =>
+      callLoader(loader, args, key),
+    ),
   )
   const data = {} as any
 
@@ -14,12 +21,65 @@ export async function callComponentLoaders(loaders: any) {
   return data
 }
 
-async function callLoader(loader: any) {
+type ComponentFunctionArgs = DataFunctionArgs & {
+  componentParams?: any
+}
+
+async function callLoader(
+  loader: any,
+  args: ComponentFunctionArgs,
+  key: string,
+) {
   try {
-    return await loader()
+    if (key) {
+      args.componentParams = getComponentParams(key)
+    }
+    return await loader(args)
   } catch (e) {
     return e
   }
+}
+
+async function callAction(
+  action: any,
+  args: ComponentFunctionArgs,
+  key: string,
+) {
+  try {
+    if (key) {
+      args.componentParams = getComponentParams(key)
+    }
+    return await action(args)
+  } catch (e) {
+    return e
+  }
+}
+
+function getComponentParams(key: string) {
+  const index = key.indexOf('?')
+  if (index < 0) return {}
+  return Object.fromEntries(new URLSearchParams(key.substring(index)))
+}
+
+export async function callComponentActions(
+  args: DataFunctionArgs,
+  actions: any,
+) {
+  const { request } = args
+  const formData = await request.formData()
+  // find component action that match the posted form data
+  if (formData.has('$$actionComponentId')) {
+    const componentId = formData.get('$$actionComponentId') as string
+    let action = actions[componentId]
+    // match component id to action (which can include :params)
+    // HACK: need to find a better way to do this
+    if (componentId.includes('?')) {
+      action = actions[componentId.substring(0, componentId.indexOf('='))]
+    }
+    const response = await await callAction(action, args, componentId)
+    return { handled: true, response }
+  }
+  return { handled: false, response: null }
 }
 
 async function getResponseData(response: any) {
